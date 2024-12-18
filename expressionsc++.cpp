3f763a49ed65302567b2667dce845f1f220a178a
mokhtari250 <168132848+mokhtari250@users.noreply.github.com>
2024-12-17 21:23:54 -0600

    Create expressionsc++.cpp
    a new way of how expressions are created on java c++

---
+++ expressionsc++.cpp
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static main void(String[] args) {
+
+        // expression = operands & operators
+        // oprands =   values, variable, numbers, quantity
+        // operators = + - * / & 
+
+        int friends = 10;
+
+        friends--;
+
+        System.out.println(friends);
+
+    }
+}
